871248a54c6fb446a7f269b6f7463a5008ecdf60
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-09-05 18:01:23 -0400

    Added sprinkler

---
+++ Objects/Sprinkler/sprinkler.gd
@@ -0,0 +1,46 @@
+class_name Sprinkler
+
+extends Node2D
+
+
+@export_group("References")
+@export var hydration_modifier_packed : PackedScene
+@export var field_row : FieldRow
+
+@export_group("Properties")
+@export var modification_amount : float = 4
+
+
+var hydration_modifier : HydrationModifier = null
+var on : bool = false
+
+
+func _ready() -> void:
+
+	hydration_modifier = hydration_modifier_packed.instantiate() as HydrationModifier
+	hydration_modifier.initialize(abs(modification_amount) * get_sign())
+
+	field_row.add_child(hydration_modifier)
+
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func get_sign() -> int:
+
+	return (2 * int(on)) - 1
+
+
+func toggle(toggle : bool) -> void:
+
+	on = toggle
+
+
+func flip() -> bool:
+
+	toggle(!on)
+	return on
+
