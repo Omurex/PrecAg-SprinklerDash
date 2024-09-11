d73ebc1a4c738769c32916c4ac8983d1cbeed4d9
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-09-11 12:41:39 -0400

    Added an extreme heat weather effect

---
+++ Objects/Weather/Effects/ExtremeHeat/extreme_heat_weather_effect.gd
@@ -0,0 +1,22 @@
+class_name ExtremeHeatWeatherEffect
+
+extends BaseWeatherEffect
+
+
+@export_group("Internal References")
+
+
+@export_group("External References")
+
+
+@export_group("Properties")
+
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
