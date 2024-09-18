83b46eced5124cb892b2259062f9a4110e851544
Joseph Lyons <joseph.lyons@mymail.champlain.edu>
2024-09-18 14:41:47 -0400

    Finished row dying notification

--- Objects/RowDiedWarningUI/row_died_warning_ui.gd
+++ Objects/RowDiedWarningUI/row_died_warning_ui.gd
@@ -23,8 +23,22 @@ func show_warning(row_num_that_died : int, current_num_dead : int, max_dead : in
 
 	visible = true
 
-	var str := "ROW " + str(row_num_that_died) + "'S CROPS HAVE DIED,\n" + \
-		str(max_dead - current_num_dead) + " CROPS LEFT BEFORE GAME OVER!"
+	var num_left := max_dead - current_num_dead
+
+	var str := "ROW " + str(row_num_that_died) + "'S CROPS HAVE DIED.\n"
+
+	if num_left == 0:
+
+		str += "GAME OVER."
+
+	else:
+
+		str += str(num_left) + " CROP"
+
+		if num_left != 1:
+			str += "S"
+
+		str += " LEFT BEFORE GAME OVER!"
 
 	label.text = str.c_unescape()
 
